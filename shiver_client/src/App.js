4fd563ea358de99aac950a15a943896a9fef797d
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-07-11 09:27:13 -0700

    rendering data in react

--- shiver_client/src/App.js
+++ shiver_client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Blog_Posts from './components/Post.js'
+import BLOG_POSTS from './components/Post.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Blog_Posts />
+      <BLOG_POSTS />
     </div>
   );
 }
--- shiver_client/src/components/Post.js
+++ shiver_client/src/components/Post.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 
-class Blog_Posts extends Component {
+class BLOG_POSTS extends Component {
+    state = {
+        blog_posts: []
+    }
+
+    componentDidMount() {
+        this.getPosts()
+    }
 
     getPosts = () =>{
-        fetch('localhost:3000')
-        .then( blog_posts => console.log(blog_posts))
+        fetch('http://localhost:3000/blog_posts')
+        .then(res => res.json())
+        .then(jsonedPosts => this.setState({ blog_posts: jsonedPosts}))
+        .catch (error => console.error(error))
       }
 
     render () {
+        console.log(this.state.blog_posts)
         return (
-            <h1>Hello World</h1>
+            <div>
+                {this.state.blog_posts.map( blog_post => {
+                    return (
+                        <div key={blog_post.id}>
+                            <h2>Title: {blog_post.title}</h2>
+                            <h3>Username: {blog_post.username}</h3>
+                            <h3>Catagory: {blog_post.catagory}</h3>
+                            <p>{blog_post.content}</p>
+                        </div>
+                    )
+                })}
+            </div>
         )
     }
 }
 
-export default Blog_Posts;
+export default BLOG_POSTS;
