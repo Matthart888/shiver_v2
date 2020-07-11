1bd84e9b7f5552026fa480a69e672b71d85f0a45
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-07-11 08:27:57 -0700

    made react app

---
+++ shiver_client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Blog_Posts from './components/Post.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Blog_Posts />
+    </div>
+  );
+}
+
+export default App;
