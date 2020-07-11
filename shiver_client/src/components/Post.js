ff84fc40192a9d1c7d43bcd5351c5db54b422c2e
Em Emm M <emm@Ems-MacBook-Pro.local>
2020-07-11 08:32:31 -0700

    configured cors

--- shiver_client/src/components/Post.js
+++ shiver_client/src/components/Post.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class Blog_Posts extends Component {
+
+    getPosts = () =>{
+        fetch('localhost:3000')
+        .then( blog_posts => console.log(blog_posts))
+      }
+
     render () {
         return (
             <h1>Hello World</h1>
